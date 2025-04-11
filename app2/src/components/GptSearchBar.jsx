2ce72b415d26de280ec2dfd35b6e20ce48eeeece
saikola24 <saikola0036@gmail.com>
2025-04-11 22:58:40 +0530

    Keys,gptPage

--- app2/src/components/GptSearchBar.jsx
+++ app2/src/components/GptSearchBar.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import lang from '../utils/languageConstants'
 import { useSelector } from 'react-redux'
-
+   
 const GptSearchBar = () => {
     const langKey=useSelector(store=>store.config.lang)
+    const searchText=useRef(null)
+    const handleGptSearchClick=()=>{
+      console.log(searchText.current.value)
+    }
   return (
     <div className='pt-[10%] flex justify-center'>
-        <form className='w-1/2 bg-black grid grid-cols-12'>
-            <input type='text' className=' p-4 m-4 bg-white rounded-lg col-span-9' placeholder={lang[langKey].gptSearchPlaceholder}/>
-            <button className='py-2 px-4 m-4 bg-red-700 text-white rounded-lg col-span-3'>{lang[langKey].search}</button>
+        <form className='w-1/2 bg-black grid grid-cols-12' onSubmit={(e)=>e.preventDefault()}>
+            <input ref={searchText} type='text' className=' p-4 m-4 bg-white rounded-lg col-span-9' placeholder={lang[langKey].gptSearchPlaceholder}/>
+            <button className='py-2 px-4 m-4 bg-red-700 text-white rounded-lg col-span-3 cursor-pointer' onClick={handleGptSearchClick}>{lang[langKey].search}</button>
         </form>
     </div>
   )
--- app2/src/utils/constants.jsx
+++ app2/src/utils/constants.jsx
@@ -2,14 +2,14 @@ export const LOGO="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-89723
 export const USER_ICON="https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
 export const BG_IMG="https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg";
 
-
 export const API_OPTIONS = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NGIxZTBmZDEzZWE2YmI5MzQ3YjhiN2E4YmM5ZGUyOCIsIm5iZiI6MTcxMTAyNTYwNi43NjE5OTk4LCJzdWIiOiI2NWZjMmRjNjA0NzMzZjAxNjRlNzM3ZWEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.o7JNAHUERn9KJMC8tvyP_X0ZQv6Z40_mrb-WLibr6Dk'
-    }
-  };
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: import.meta.env.VITE_TMDB_AUTH_KEY
+  }
+};
+
 
 export const IMG_CDN_URL="https://image.tmdb.org/t/p/w300"
   
